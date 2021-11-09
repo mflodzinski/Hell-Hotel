d887305cc8ace7a6bb6a8f1f5386584545b8ec98
atrwae <artur.midiaibim@gmail.com>
2021-11-09 21:35:37 +0100

    Chodzenie i obracanie się.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
-     
+    public float rotationSpeed = 400;
+
 
     void Update()
     {
         Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         transform.Translate(inputVector * Time.deltaTime * speed);
 
-        transform.Rotate((Input.GetAxis("Mouse Y") * 100 * Time.deltaTime), (Input.GetAxis("Mouse X") * 100 * Time.deltaTime), 0);
+        transform.Rotate(-(Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime), 0, 0, Space.Self);
+        transform.Rotate(0, -(Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime), 0, Space.World);
     }
 }
