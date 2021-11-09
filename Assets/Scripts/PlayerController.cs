dbe224d94a4700fdb37af9a66f1d44bb3b0d042d
atrwae <artur.midiaibim@gmail.com>
2021-11-09 22:56:55 +0100

    Drobna zmiana, żeby się nie dało latać.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float temp = transform.position.y;
         transform.Translate(inputVector * Time.deltaTime * speed);
+        transform.position = new Vector3(transform.position.x, temp, transform.position.z);
 
         transform.Rotate(-(Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime), 0, 0, Space.Self);
-        transform.Rotate(0, -(Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime), 0, Space.World);
+        transform.Rotate(0, (Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime), 0, Space.World);
     }
 }
