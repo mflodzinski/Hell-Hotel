23db5a658d330b1731066db396d2bf716326e0df
atrwae <artur.midiaibim@gmail.com>
2021-11-10 15:32:58 +0100

    Drobne popraweczki

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
     public float rotationSpeed = 400;
+    public Transform cam;
 
 
     void Update()
     {
         Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        float temp = transform.position.y;
         transform.Translate(inputVector * Time.deltaTime * speed);
-        transform.position = new Vector3(transform.position.x, temp, transform.position.z);
 
-        transform.Rotate(-(Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime), 0, 0, Space.Self);
+        Quaternion previousCamRotation = cam.rotation;
+        cam.Rotate(-(Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime), 0, 0, Space.Self);
+        if (Vector3.Angle(cam.forward, transform.forward) > 90) cam.rotation = previousCamRotation;
+
         transform.Rotate(0, (Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime), 0, Space.World);
     }
 }
