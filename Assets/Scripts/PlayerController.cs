cd9b57281b869d0f7f21ccade13db35abb5bcd7d
@rickimanda <a.krawczuk1@gmail.com>
2021-11-10 22:01:20 +0100

    implemented jump and cursor hiding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 10;
     public float rotationSpeed = 400;
     public Transform cam;
+    public Rigidbody playerBody;
+    public float jumpHeight = 10;
+    bool isGrounded;
+    bool jumpPressed;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
 
     void Update()
@@ -19,5 +28,21 @@ public class PlayerController : MonoBehaviour
         if (Vector3.Angle(cam.forward, transform.forward) > 90) cam.rotation = previousCamRotation;
 
         transform.Rotate(0, (Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime), 0, Space.World);
+
+        jumpPressed = Input.GetButtonDown("Jump") || Input.GetButton("Jump") ;
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumpPressed && isGrounded)
+        {
+            playerBody.velocity = new Vector3(0, jumpHeight, 0);
+            isGrounded = false;
+        }
     }
 }
