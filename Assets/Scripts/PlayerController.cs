e4217fef5db8219efdd44b14e6f08bd223c6fd5b
atrwae <artur.midiaibim@gmail.com>
2022-01-22 15:06:57 +0100

    Fixed walking through walls.. I think, maybe? idk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10;
-    public float sprintSpeed = 15;
+    public float walkAcceleration = 10;
+    public float sprintAcceleration = 15;
     public float rotationSpeed = 400;
+    [SerializeField]
+    private float maxWalkingSpeed = 4;
+    [SerializeField]
+    private float maxSprintingSpeed = 7;
 
     public Transform cam;
     public Rigidbody playerBody;
@@ -13,9 +17,10 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 10;
     bool isGrounded;
     public float gravity = 9.81f;
+    [SerializeField]
+    private float coefficient = 10;
 
     GunController gunController;
-    Vector3 velocity;
 
     private void Start()
     {
@@ -25,32 +30,13 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    void FixedUpdate()
+    void Update()
     {
-        Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        
-        if (Input.GetButton("Sprint") && isGrounded)
-        {
-            transform.Translate(inputVector * Time.deltaTime * sprintSpeed);
-
-        }
-        else
-        {
-            transform.Translate(inputVector * Time.deltaTime * speed);
-        }
-
         Quaternion previousCamRotation = cam.rotation;
         cam.Rotate(-(Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime), 0, 0, Space.Self);
         if (Vector3.Angle(cam.forward, transform.forward) > 90) cam.rotation = previousCamRotation;
 
         transform.Rotate(0, (Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime), 0, Space.World);
-
-        if ((Input.GetButtonDown("Jump") || Input.GetButton("Jump")) && isGrounded)
-        {
-            velocity = new Vector3(0, Mathf.Sqrt(2 * jumpHeight * gravity), 0);
-        }
-        velocity.y -= gravity * Time.deltaTime;
-        playerBody.velocity = velocity;
 
         //Mouse input
         if (Input.GetMouseButton(0))
@@ -59,10 +45,33 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float xzVelocityLength = Mathf.Sqrt(playerBody.velocity.x * playerBody.velocity.x + playerBody.velocity.z * playerBody.velocity.z);
+
+        if (Input.GetButton("Sprint"))
+        {
+            //Sprinting
+            if (xzVelocityLength < maxSprintingSpeed) playerBody.AddRelativeForce(inputVector * Time.fixedDeltaTime * sprintAcceleration * 6, ForceMode.VelocityChange);
+        }
+        else
+        {
+            //Walking
+            if (xzVelocityLength < maxWalkingSpeed) playerBody.AddRelativeForce(inputVector * Time.fixedDeltaTime * walkAcceleration * 6, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            playerBody.AddRelativeForce(new Vector3(0, jumpHeight, 0), ForceMode.VelocityChange);
+        }
+        
+        playerBody.AddRelativeForce(new Vector3(0, -gravity, 0), ForceMode.Acceleration);
+    }
+
     private void OnCollisionStay()
     {
         isGrounded = true;
-        velocity = Vector3.zero;
     }
     private void OnCollisionExit()
     {
